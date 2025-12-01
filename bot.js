2c97a468706df579db0bdb85c116729d59d1ef8d
Bohdan <121368986+bdiev@users.noreply.github.com>
2025-12-01 15:33:32 +0200

    Update avatar URL generation to use Minotar service for Discord notifications

--- bot.js
+++ bot.js
@@ -672,7 +672,7 @@ function createBot() {
     try {
       const channel = await discordClient.channels.fetch(DISCORD_CHAT_CHANNEL_ID);
       if (channel && channel.isTextBased()) {
-        let avatarUrl = `https://crafatar.com/avatars/${username.toLowerCase()}?size=64&default=MHF_Steve&overlay`;
+        let avatarUrl = `https://minotar.net/avatar/${username.toLowerCase()}/64`;
         await channel.send({
           embeds: [{
             author: {
